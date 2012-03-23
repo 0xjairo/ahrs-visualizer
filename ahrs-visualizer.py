52a002d3cb0a46b372f3eaf510e8f8219c9d3cea
Jairo <jairoscz@gmail.com>
2012-03-23 17:31:41 -0500

    better documentation

--- ahrs-visualizer.py
+++ ahrs-visualizer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# Code downloaded from on MAR-2012: 
+# http://edge.rit.edu/content/P11015/public/Visual%20Python%20for%20Gyroscope%20Utilization.py
+# The original code has been modified to read the output from the tricopter at
+# https://github.com/jyros/tricopter
 
 # Test for Razor 9DOF IMU
 # Jose Julio @2009
@@ -9,22 +13,22 @@
 # Install pyserial from http://sourceforge.net/projects/pyserial/files/
 # Install Vphyton from http://vpython.org/contents/download_windows.html
 
-# downloaded from: http://edge.rit.edu/content/P11015/public/Visual%20Python%20for%20Gyroscope%20Utilization.py
 from visual import *
 import serial
 import string
 import math
+import sys
+import datetime
 
-from time import time
-
-grad2rad = 3.141592/180.0
+# LeafLabs Maple serial port
+serialport = '/dev/ttyacm0'
 
 # Check your COM port and baud rate
 try:
-    ser = serial.Serial(port='COM4',baudrate=57600, timeout=1)
+    ser = serial.serial(serialport)
 except serial.SerialException:
-    #ser = open('screenlog.0', 'r')
-    ser = open('log-short.0', 'r')
+    print "Serial port not found"
+    sys.exit(0)
 
 # Main scene
 scene=display(title="9DOF Razor IMU test")
@@ -43,6 +47,8 @@ scene2.select()
 #Roll, Pitch, Yaw
 cil_roll = cylinder(pos=(-0.4,0,0),axis=(0.2,0,0),radius=0.01,color=color.red)
 cil_roll2 = cylinder(pos=(-0.4,0,0),axis=(-0.2,0,0),radius=0.01,color=color.red)
+cil_roll_bg = cylinder(pos=(-0.4,0,0),axis=(0.2,0,0),radius=0.01,color=(1,1,1),opacity=0.25)#color.yellow)
+cil_roll2_bg = cylinder(pos=(-0.4,0,0),axis=(-0.2,0,0),radius=0.01,color=(1,1,1),opacity=0.25)#color.yellow)
 cil_pitch = cylinder(pos=(0.1,0,0),axis=(0.2,0,0),radius=0.01,color=color.green)
 cil_pitch2 = cylinder(pos=(0.1,0,0),axis=(-0.2,0,0),radius=0.01,color=color.green)
 #cil_course = cylinder(pos=(0.6,0,0),axis=(0.2,0,0),radius=0.01,color=color.blue)
@@ -82,19 +88,21 @@ platform = box(length=1, height=0.05, width=1, color=color.red)
 p_line = box(length=1,height=0.08,width=0.1,color=color.yellow)
 plat_arrow = arrow(color=color.green,axis=(1,0,0), shaftwidth=0.06, fixedwidth=1)
 
+now = datetime.datetime.now()
 
-f = open("Serial"+str(time())+".txt", 'w')
+f = open("Serial_"+ now.strftime("%Y%m%d%H%M%S") +".txt", 'w')
 
 roll=0
 pitch=0
 yaw=0
-while 1:
-    rate(10)
 
-    line = ser.readline()
+line = ser.readline()
+while len(line)>0:
+    #rate(10)
+
     #line = line.replace("!RAW:","")   # Delete "!ANG:"
     #print line
-    #f.write(line)                     # Write to the output log file
+    f.write(line)                     # Write to the output log file
 
     # split line
     s = line.split(' ')
@@ -107,22 +115,19 @@ while 1:
             d[kv[0]] = kv[1]
 
     try:
-        roll = float(d['y'])
+        roll = round(radians(float(d['yRoll'])),2)
+        pitch = round(radians(float(d['yPitch'])),2)
+        yaw = radians(180) #round(radians(float(d['yYaw'])),2)
+        uroll = round(radians(float(d['uRoll'])),2)
+        #print line,
     except KeyError:
+        print 'invalid line'
         roll = 0
+        pitch = 0
+        yaw = radians(180)
         ''' do nothing '''
 
-    pitch = 0
-    yaw = 0
 
-    #words = string.split(line," ")    # Fields split
-    #if len(words) > 2:
-    #    try:
-    #        roll = float(words[0])*grad2rad
-    #        pitch = float(words[1])*grad2rad
-    #        yaw = float(words[2])*grad2rad
-    #    except:
-    #        print "Invalid line"
     if True:
         axis=(cos(pitch)*cos(yaw),-cos(pitch)*sin(yaw),sin(pitch)) 
         up=(sin(roll)*sin(yaw)+cos(roll)*sin(pitch)*cos(yaw),sin(roll)*cos(yaw)-cos(roll)*sin(pitch)*sin(yaw),-cos(roll)*cos(pitch))
@@ -137,6 +142,8 @@ while 1:
         p_line.up=up
         cil_roll.axis=(0.2*cos(roll),0.2*sin(roll),0)
         cil_roll2.axis=(-0.2*cos(roll),-0.2*sin(roll),0)
+        cil_roll_bg.axis=(0.2*cos(uroll),0.2*sin(uroll),0)
+        cil_roll2_bg.axis=(-0.2*cos(uroll),-0.2*sin(uroll),0)
         cil_pitch.axis=(0.2*cos(pitch),0.2*sin(pitch),0)
         cil_pitch2.axis=(-0.2*cos(pitch),-0.2*sin(pitch),0)
         arrow_course.axis=(0.2*sin(yaw),0.2*cos(yaw),0)
@@ -146,5 +153,9 @@ while 1:
         #L1.text = str(float(words[0]))
         #L2.text = str(float(words[1]))
         #L3.text = str(float(words[2]))        
+    line = ser.readline()
+
+print 'done'
 ser.close
 f.close
+
