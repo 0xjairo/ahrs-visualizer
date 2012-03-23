21aad7448be8ae49d2c08953a340fa3a81f39a94
Jairo <jairoscz@gmail.com>
2012-03-23 17:35:09 -0500

    ...trying to create a geometric representation of the airframe

--- vis.py
+++ vis.py
@@ -3,13 +3,20 @@
 import visual as v
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.timeout=0
+try:
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    ser.timeout=0
+except : #SerialException:
+    ser = None
 
-ball = v.sphere(pos=(-6,0,0), radius=0.5, color=v.color.cyan)
+if ser == None:
+    #f = open('log-short.0', 'r')
+    f = open('screenlog.0', 'r')
+
+#ball = v.sphere(pos=(-6,0,0), radius=0.5, color=v.color.cyan)
 copter = v.box(pos=(0,-5,0), size=(5,0.2,3), color=v.color.green)
 
-ball.velocity = v.vector(25,0,0)
+#ball.velocity = v.vector(25,0,0)
 vscale = 1
 
 # x arrow
@@ -18,22 +25,46 @@ xarr = v.arrow(pos=copter.pos, axis=vscale*copter.pos, color=v.color.yellow)
 deltat = 0.005
 t = 0
 
-v.scene.autoscale = False
-while t<3:
-
-    v.rate(100)
-
+if ser == None:
+    line =  f.readline()
+else:
     line =  ser.readline()
-    if len(line) > 0:
-        print line
 
-    if ball.pos.x > 6 or ball.pos.x < -6:
-        ball.velocity.x = -ball.velocity.x
+v.scene.autoscale = False
+i=0
+while len(line)>0:
 
-    #xarr.pos = ball.pos
-    #xarr.axis = vscale*ball.velocity
+    v.rate(10)
 
-    ball.pos = ball.pos + ball.velocity*deltat
+    if ser == None:
+        line =  f.readline()
+    else:
+        line =  ser.readline()
+
+    s = line.split(' ')
+    d = dict()
+    for token in s:
+        kv = token.split(':')
+        if(kv != -1 and len(kv) == 2):
+            d[kv[0]] = kv[1]
+
+    try:
+        roll = float(d['y'])
+    except KeyError:
+        roll = 0
+        ''' do nothing '''
+
+    print roll
+
+    #copter.rotate(angle=v.radians(i))
+    copter.rotate(angle=v.radians(roll))
+    i += 0.1
     t = t + deltat
 
-ser.close()
+print 'Done!'
+
+if ser == None:
+    f.close()
+else:
+    ser.close()
+
